3d3e45c127662a5f682a6993ea42b0fa30aa4052
Alicia Zamorano <zamoranoalicia@gmail.com>
2024-06-27 13:25:55 -0400

    QS-006: Adding interpreter.

--- resources/code.qs
+++ resources/code.qs
@@ -1,6 +1,12 @@
 PROGRAM test;
 VAR
-    xyz:INTEGER;
+    xyz,abc:INTEGER;
+    cdf,wvy :INTEGER;
 BEGIN
-    xyz := 2+7; 
+    xyz := 2+7;
+    cdf := 3+5;
+END.
+BEGIN
+    abc := 2+7;
+    wvy := 3+5;
 END.
--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,6 +1,16 @@
 module Interpreter where
 
 import qualified AST as AST
+import qualified SymbolTable as ST
+import qualified SemanticAnalyzer as SA
+import qualified Data.Map.Strict as M
+
+
+data InterpreterOutput = Environment
+                        { 
+                            values :: M.Map String Int
+                        }
+                        deriving (Show, Eq)
 
 interpretFactor :: AST.Factor -> Int
 interpretFactor (AST.Value v) = v
@@ -13,7 +23,23 @@ interpretExpression (AST.Plus x y ) = (interpretTermFactor x) + (interpretTermFa
 interpretExpression (AST.Minus x y ) = (interpretTermFactor x) - (interpretTermFactor y)
 interpretExpression (AST.ETerm t) = interpretTermFactor t
 
-interpretStatement :: AST.Statement -> Int
-interpretStatement (AST.Assign _ exp) = interpretExpression exp
-interpretStatement (AST.EmptyStatement) = 0
+interpretStatement :: AST.Statement -> ST.SymbolTable -> (String, Int)
+interpretStatement (AST.Assign id exp) table = (ST.nameFromIdentifier id, getExpValue id exp table)
+    where
+        getExpValue id expression symbolTable =
+            let name = ST.nameFromIdentifier id
+                symbol = (ST.lookupSymbol name symbolTable)
+            in case symbol of
+                Just (ST.Symbol _ _ value) -> (ST.valueFromSymbolValue value)
+                Nothing -> (interpretExpression expression)
+interpretStatement (AST.EmptyStatement) _ = ("",0)
 
+interpreCompStatement :: AST.CompoundStatement -> ST.SymbolTable -> [(String, Int)]
+interpreCompStatement (AST.CompoundStatement stmts) table = map (`interpretStatement` table) stmts
+
+interpretBlock :: AST.Block -> ST.SymbolTable -> [[(String, Int)]]
+interpretBlock block@(AST.Block declaration compStatements) table = updatedTable block table
+            where
+                updatedTable block table =
+                    let (blockNode, newTable) = (SA.analyzeBlock block table)
+                    in map (`interpreCompStatement` newTable) compStatements
